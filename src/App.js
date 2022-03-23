42bf658c889ff7219f155a8878b45c0000e5e1d9
gabo-x86 <hurtadogabriel58@gmail.com>
2022-03-23 18:19:22 -0400

    TestComponent created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Test';
+import TestComponent from './components/Test';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TestComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
